6ada91094fb548fbb7af022f647856dbf76dc937
KavetiRohith <KavetiRohith>
2024-01-18 20:05:20 -0800

    move on from the ZIG_SYSTEM_LINKER_HACK flag

--- build.sh
+++ build.sh
@@ -5,10 +5,9 @@ set -eux
 rm -f bootstrap lambda-handler.zip
 export GOOS=linux
 export CGO_ENABLED=1
-export CC=$(pwd)/zcc.sh
-export CXX=$(pwd)/zxx.sh
-
-GOARCH=amd64 \
-ZTARGET=x86_64-linux-musl \
+export CC="zig cc -target x86_64-linux-musl"
+export CXX="zig c++ -target x86_64-linux-musl"
+export GOARCH=amd64
 go build -tags musl -ldflags="-linkmode external" -o bootstrap
 zip lambda-handler.zip bootstrap
+rm bootstrap
